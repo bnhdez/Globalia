e8e0f9c71ed14e67ff2edb90040b3257fe4f559c
borishdez <borisnandez93@gmail.com>
2024-06-18 11:52:34 -0600

    aboutpage component creado

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/homePage/homePage.component';
+import { AboutPageComponent } from './pages/aboutPage/aboutPage.component';
 
 
 
 @NgModule({
   declarations: [
+    AboutPageComponent,
     HomePageComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
+    AboutPageComponent,
     HomePageComponent
   ]
 })
