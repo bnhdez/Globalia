6273e6a22494fd198db2c5b13e4a2180e00c50d8
borishdez <borisnandez93@gmail.com>
2024-06-19 20:42:50 -0600

    observable http de interfaz country

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
 export class ServiceNameService {
-  constructor(private httpClient: HttpClient) { }
 
+  private apiUrl: string = 'https://restcountries.com/v3.1';
+
+  constructor(private http: HttpClient) { }
+
+  //term es el termino de la capital a buscar
+  //http es tipo observable que requiere interfaz si no se especifica, regresa objeto sin metodos
+  searchCapital( term:string ):Observable<Country[]>{
+    const url = `${this.apiUrl}/capital/${term}`;
+    // debo especificar que tipo de dato retornar
+    return this.http.get<Country[]>( url );
+  }
 }
