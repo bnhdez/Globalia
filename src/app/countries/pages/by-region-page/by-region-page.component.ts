cc5c883bd5f47fbab96e64b2e0f545bbdc4d11bf
borishdez <borisnandez93@gmail.com>
2024-06-22 13:08:06 -0600

    type Regions Estricto

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { countriesService } from '../../services/countries.service';
 
+//regiones permitidas para busqueda
+type Region = 'Africa'|'Americas'|'Asia'|'Europe'|'Oceania';
+
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -10,6 +13,7 @@ import { countriesService } from '../../services/countries.service';
 export class ByRegionPageComponent {
 
   public countries: Country[] = [];
+  public regions:Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania']
 
   constructor(private countriesService: countriesService) { }
 
