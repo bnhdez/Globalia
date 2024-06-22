00aa62a6ba146a7885b79304b9fadee5107e3e90
borishdez <borisnandez93@gmail.com>
2024-06-21 19:59:34 -0600

    recibo country by alpha code en observable

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { countriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'countries-country-page',
@@ -8,11 +9,20 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CountryPageComponent implements OnInit {
 
-  constructor ( private activatedRoute:ActivatedRoute ) { }
+  constructor (
+    private activatedRoute:ActivatedRoute,
+    private countriesService:countriesService
+  ) { }
+
   ngOnInit(): void {
+    // observable dentro de otro observable
+    // debo suscribit para manipular y asignar la data
     this.activatedRoute.params
       .subscribe( ({id}) => {
-        console.log({ params: id });
+        this.countriesService.searchCountryByAlphaCode(id)
+          .subscribe( country => {
+            console.log({country});
+          })
       })
   }
 
