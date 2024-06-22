c11117a10694c16443618547dc538546df14a4f5
borishdez <borisnandez93@gmail.com>
2024-06-22 11:52:38 -0600

    getCountriesRequest refactorizar metodos en service

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, map, of } from 'rxjs';
+import { Observable, catchError, delay, map, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
@@ -9,6 +9,18 @@ export class countriesService {
   private apiUrl: string = 'https://restcountries.com/v3.1';
 
   constructor(private http: HttpClient) { }
+
+  // recibo url especifico segun metodos abajo, excepto alphacode
+  private getCountriesRequest( url:string ):Observable<Country[]>{
+    return this.http.get<Country[]>( url )
+      .pipe(
+        catchError(error => {
+          console.log(error);
+          return of([])
+        }),
+        delay( 1000 )
+      )
+  }
 
   // no quiero que regrese el arreglo de todos, solo un country o null
   searchCountryByAlphaCode(code: string): Observable<Country | null>{
@@ -29,15 +41,16 @@ export class countriesService {
   //http es tipo observable que requiere interfaz si no se especifica, regresa objeto sin metodos
   searchCapital( term:string ):Observable<Country[]>{
     const url = `${this.apiUrl}/capital/${term}`;
+    return this.getCountriesRequest( url );
     // debo especificar que tipo de dato retornar
     // of va a regresar un nuevo observable (array vacio) con el error
-    return this.http.get<Country[]>( url )
-      .pipe(
-        catchError( error => {
-          console.log(error);
-          return of([])
-        })
-      )
+    // return this.http.get<Country[]>( url )
+    //   .pipe(
+    //     catchError( error => {
+    //       console.log(error);
+    //       return of([])
+    //     })
+    //   )
     // return this.http.get<Country[]>( url )
     //   .pipe(
     //     tap( countries => console.log('Tap1', countries)),
@@ -48,23 +61,11 @@ export class countriesService {
 
   searchCountry( term:string ):Observable<Country[]>{
     const url = `${this.apiUrl}/name/${term}`;
-    return this.http.get<Country[]>( url )
-      .pipe(
-        catchError( error => {
-          console.log(error);
-          return of([]);
-        })
-      )
+    return this.getCountriesRequest(url);
   }
 
   searchRegion( region:string ):Observable<Country[]>{
     const url = `${this.apiUrl}/region/${region}`;
-    return this.http.get<Country[]>(url)
-      .pipe(
-        catchError(error => {
-          console.log(error);
-          return of([]);
-        })
-      )
+    return this.getCountriesRequest(url);
   }
 }
