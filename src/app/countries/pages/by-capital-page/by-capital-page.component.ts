8d32b0d8eb817433dfa825edd3ca83e25826bf2f
borishdez <borisnandez93@gmail.com>
2024-06-22 12:19:15 -0600

    ngIf isLoading

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -10,17 +10,21 @@ import { Country } from '../../interfaces/country';
 export class ByCapitalPageComponent {
 
   public countries:Country[] = []
+  public isLoadign:boolean = false;
 
   constructor( private countriesService:countriesService ) { }
 
   //recibe value emitido desde searchBoxComponent y ahora es term
   searchByCapital( term:string ):void{
-
+    // esta cargando la busqueda
+    this.isLoadign = true;
     //.subscribe es un metodo para un observable que deja
     //permite manjar los datos del observable
     this.countriesService.searchCapital( term )
       .subscribe( countries =>{
         this.countries = countries;
+        // ya tiene los resultados
+        this.isLoadign = false;
       } );
   }
 }
