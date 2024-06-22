4ace9446baff26892114cb652e8f461cef7dc251
borishdez <borisnandez93@gmail.com>
2024-06-21 19:49:00 -0600

    country-page constructor and ngOnInit

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'countries-country-page',
   templateUrl: './country-page.component.html',
   styles: ``
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+
+  constructor ( private activatedRoute:ActivatedRoute ) { }
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .subscribe( params => {
+        console.log({ params: params['id'] });
+      })
+  }
+
 
 }
